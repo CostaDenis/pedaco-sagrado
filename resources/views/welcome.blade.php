84abfb1935517a51d9f7e983a7e1ef399bf0a5e1
Victor Yazigi Saliba <yazigi@MacBook-Pro-de-Victor.local>
2025-12-25 21:14:36 -0300

    Initial Laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ola mundo</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <span class='bg-red-100 text-3xl'>TESTANDO ALTAMENTE O LARAVEL</span>
+</body>
+</html>
